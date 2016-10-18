2fed77a46937d32b6cf3480460171ece9d6f4b5e
bacpiano <borceapiano@yahoo.com>
2016-10-18 15:30:39 -0700

    This is from the PirateFleet1 (Variable or Constant?) section of the Beginning iOS Development nano degree from Udacity.

---
+++ Variables Exercises.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: ## Variables Exercises
+//: In these exercises, you will create variables and constants, and you will handle simple compiler areas that arise when working with them.
+/*:
+### Exercise 1
+
+For each of the following names and types, create a variables or constant that best represents it:
+
+- boilingPointOfWaterInCelsius (Double)
+- gallonsRemainingInTank (Double)
+- naturalLogarithmicBase (Double)
+- ageOfSpecimenInWeeks (Int)
+- studentNickname (String)
+*/
+
+var biolingPointOfWaterInCelsius: Double = 100
+var gallonsRemainingInTank: Double = 5.4
+var naturalLogarithmicBase: Double = 10.0
+var ageOfSpecimenInWeeks: Int = 4
+var studentNickname: String = "sebastian bach"
+
+//: ### Exercise 2
+//: Fix the following compiler errors related to assignment, variables, and constants:
+var favoriteLanguage: Character = "s"
+gallonsRemainingInTank = 2
+ageOfSpecimenInWeeks = 32
+let _numberOfGasIcons: Double = gallonsRemainingInTank
+var numberOfPolygonsInModel = 30
+numberOfPolygonsInModel = 32
+var polygonsLeftToDraw = 0
+polygonsLeftToDraw = numberOfPolygonsInModel
+
+//: ### Exercise 3
+//: Fix the following compiler errors related to invalid names:
+var nameOfSchool = "Udacity"
+var schoolClass = "Math"
+let important = "Nissian"
+var 🐩myDog = "🐶"
+var 🐕yourDog = "🐶"
+
+
+
---
+++ Variables.playground/Pages/Using Playgrounds.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+//: ## Using Playgrounds
+//: Playground - noun: a place where people can play
+import UIKit // to import a User Interface/object library.
+
+var str = "Hello, playground" /* semicolons are not necessary at the
+ ends of statements. Rather - new lines define the beginning of new
+ statements. */
+
+// This is a single line comment
+
+/* This is a
+    multi-line comment. 
+
+You can write whatever
+comments you want between the opening slash/star and
+the closing star/slash.
+
+*/
+/*:
+This multi-line comment uses Markup!
+*/
+//: [Next](@next)
